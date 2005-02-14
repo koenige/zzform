7501a7d3d4e7402e4cf6b7c0223b529268d33a8f
Gustaf Mossakowski <web@koenige.org>
2005-02-14 16:55:00 +0000

    geo form: instead of outputting HTML code, writes it into string
    git-svn-id: file:///Repos/zzform/trunk@17 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- editinc/coords-edit.inc.php
+++ editinc/coords-edit.inc.php
@@ -15,40 +15,33 @@ function print_editform($form_coords, $coords) {
 	$form_coords_split = explode("[", $form_coords);
 	$form_coords_ll = $form_coords_split[1];
 
-?>
-<input type="text" size="3" maxlength="3" name="<?php echo $form_coords; ?>][deg]" id="<?php echo $form_coords; ?>][deg]" value="<?php echo $coords[$form_coords_ll]['deg'];?>"> &deg;
-<input type="text" size="3" maxlength="3" name="<?php echo $form_coords; ?>][min]" id="<?php echo $form_coords; ?>][min]" value="<?php echo $coords[$form_coords_ll]['min'];?>"> '
-<input type="text" size="4" maxlength="4" name="<?php echo $form_coords; ?>][sec]" id="<?php echo $form_coords; ?>][sec]" value="<?php echo $coords[$form_coords_ll]['sec'];?>"> &quot;
-<?php 
+	$output = '';
+	$output .= '<input type="text" size="3" maxlength="3" name="'.$form_coords.'][deg]" id="'.$form_coords.'][deg]" value="'.$coords[$form_coords_ll]['deg'].'"> &deg;';
+	$output .= '<input type="text" size="3" maxlength="3" name="'.$form_coords.'][min]" id="'.$form_coords.'][min]" value="'.$coords[$form_coords_ll]['min'].'"> \'';
+	$output .= '<input type="text" size="4" maxlength="4" name="'.$form_coords.'][sec]" id="'.$form_coords.'][sec]" value="'.$coords[$form_coords_ll]['sec'].'"> &quot;';
 
 	if ($form_coords_ll == "lat") {
 		$orientations = array('+' => 'N', '-' => 'S');
 	} elseif ($form_coords_ll == "lon") {
 		$orientations = array('+' => 'E', '-' => 'W');
 	} else {
-		echo "Programmer's fault. Variable must have lat or lon in its name";
+		$output.= "Programmer's fault. Variable must have lat or lon in its name";
 	}
 	$text_pos = "text_".$orientations['+'];
 	$text_neg = "text_".$orientations['-'];
 
-?>
-<select name="<?php echo $form_coords; ?>][orientation]" id="<?php echo $form_coords; ?>][orientation]" size="1">
-	<option <?php 
-	
+	$output.= '<select name="'.$form_coords.'][orientation]" id="'.$form_coords.'][orientation]" size="1">'."\n";
+	$output.= '<option '; 
 	if ($coords[$form_coords_ll]['orientation'] == $orientations['+']) {
-		echo "selected ";
+		$output.= "selected ";
 	}
-	
-	 ?>value="+"><?php echo $$text_pos; ?></option>
-	<option <?php 
-	
+	$output.= 'value="+">'.$$text_pos.'</option>';
+	$output.= '<option '; 
 	if ($coords[$form_coords_ll]['orientation'] == $orientations['-']) {
-		echo "selected ";
+		$output.= "selected ";
 	}
-	
-	 ?>value="-"><?php echo $$text_neg; ?></option>
-</select>
-
-<?php
+	$output.= 'value="-">'.$$text_neg.'</option>
+</select>';
+	return $output;
 }
 ?>
