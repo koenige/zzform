bcc3b1509ad3f8d9fc28e0006977b7381fb96322
Gustaf Mossakowski <web@koenige.org>
2011-03-23 16:45:18 +0000

    Removed old example files in local.
    git-svn-id: file:///Repos/zzform/trunk@107 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- local/db.inc.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-include "/Users/pwd.inc";
-
-$zz_conf['db_name'] = 'gustaf';
-$zz_conf['db_connection'] = mysql_connect($db_host, $db_user, $db_passwort); #datenbank verbindung aufbauen
-
-?>
