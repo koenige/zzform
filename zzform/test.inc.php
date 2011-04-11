bb8c8e991338ee71e662498644e90c22569f3919
Gustaf Mossakowski <web@koenige.org>
2011-04-11 09:29:18 +0000

    Test-Modul für zzform, einzelne Testszenarien formuliert
    git-svn-id: file:///Repos/zzform/trunk@139 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ zzform/test.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+// zzform scripts (Zugzwang Project)
+// (c) Gustaf Mossakowski <gustaf@koenige.org>, 2011
+// Tests
+
+
+/**
+ * Upload Module
+ *
+ * 1) upload JPEG, GIF or PNG
+ * 2) upload TIFF (different thumbnail format)
+ * 3) upload RAW
+ * 4) upload DWG -- file
+ * 5) upload PDF
+ * 6) upload AI -- like PDF but extension is important
+ *
+ * - upload with 'auto'
+ * - upload with 'source_file'
+ * - upload unknown file, upload not matching file (backup dir filled?)
+ * - upload with debug, read debug messages
+ */
+
+
+/**
+ * Validation Module
+ *
+ */
+
+?>
