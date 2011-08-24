1d6283a7a7adeac2ae48c920bda103bf7e21540e
Gustaf Mossakowski <web@koenige.org>
2011-08-24 13:15:00 +0000

    Darstellung Suchergebnisse in zz_mark_search_string() jetzt inkorrekt ohne Leerzeichen
    git-svn-id: file:///Repos/zzform/trunk@342 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzform/list.inc.php
+++ zzform/list.inc.php
@@ -1300,7 +1300,7 @@ function zz_mark_search_string($value, $field_name = false, $field = array()) {
 	}
 
 	// meta characters which must be escaped for preg_replace
-	$needle = preg_quote($_GET['q']);
+	$needle = preg_quote(trim($_GET['q']));
 	$highlight = '<span class="highlight">\1</span>';
 	$pattern = '#(?!<.*?)(%s)(?![^<>]*?>)#i';
 	$regex = sprintf($pattern, $needle);
