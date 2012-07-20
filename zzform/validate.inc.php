17f212a63c6c45365afe1dc1886eb477f4193dbe
Gustaf Mossakowski <web@koenige.org>
2012-07-20 07:13:54 +0000

    zzform: Funktion datum_int() von numbers.inc.php nach validate.inc.php verschoben
    git-svn-id: file:///Repos/zzform/trunk@596 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzform/validate.inc.php
+++ zzform/validate.inc.php
@@ -267,4 +267,98 @@ function zz_check_time($time) {
 	return $time;
 }
 
+/**
+ * converts user input date into international date string
+ * 
+ * @param string $datum date in several possible formats
+ * @return string international date YYYY-MM-DD
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ */
+function datum_int($datum) {
+	if ($datum) {
+		$months = array(
+			1 => array('Januar', 'January', 'janvier', 'Jan', 'I'),
+			2 => array('Februar', 'February', 'février', 'Feb', 'F.vrier', 'II'),
+			3 => array('März', 'March', 'mars', 'Mar', 'M.r', 'M.rz', 'III'),
+			4 => array('April', 'avril', 'Apr', 'IV'),
+			5 => array('Mai', 'May', 'fevrier', 'V'),
+			6 => array('Juni', 'June', 'juin', 'Jun', 'VI'),
+			7 => array('Juli', 'July', 'juilett', 'Jul', 'VII'),
+			8 => array('August', 'août', 'ao.t', 'Aug', 'VIII'),
+			9 => array('September', 'Septembre', 'Sep', 'IX'),
+			10 => array('Oktober', 'October', 'octobre', 'Oct', 'Okt', 'X'),
+			11 => array('November', 'novembre', 'Nov', 'XI'),
+			12 => array('Dezember', 'December', 'décembre', 'd.cembre', 'Dec', 'Dez', 'XII')
+		);
+		$datum = trim($datum); // remove unnecessary whitespace
+		foreach ($months as $month => $values)
+			foreach ($values as $value)
+				if (preg_match('/^[^a-z]*'.$value.'[^a-z]/i', $datum)) {
+					$datum = preg_replace('/'.$value.'/i', $month, $datum);
+					$newdate['month_a'] = $month;
+				}
+		if (preg_match('/^[0-9]{6}$/', $datum)) {// 250376 or 032576 or 760325
+			$newdate['day'] = substr($datum, 0, 2);
+			$newdate['month'] = substr($datum, 2, 2);
+			$newdate['year'] = substr($datum, 4, 2);
+			if ($newdate['month'] > 12) {
+				$newdate['day'] = substr($datum, 2, 2);
+				$newdate['month'] = substr($datum, 0, 2);
+			} elseif ($newdate['day'] > 31) {
+				$newdate['year'] = substr($datum, 0, 2);
+				$newdate['day'] = substr($datum, 4, 2);
+			}
+		} elseif (preg_match('/^[0-9]{8}$/', $datum)) { // 25031976
+			$newdate['day'] = substr($datum, 0, 2);
+			$newdate['month'] = substr($datum, 2, 2);
+			$newdate['year'] = substr($datum, 4, 4);
+		} elseif (preg_match('/^([0-9]{1,4})[^0-9][ ]*([0-9]{1,2})[^0-9][ ]*([0-9]{1,5})/', $datum, $treffer)) {
+			// number 
+			if (strlen($treffer[3]) > 4) return false; // error, should be a re-entry
+			if (!(empty($newdate['month_a'])) && ($newdate['month_a'] != $treffer[2])) {
+				$treffer['bak'] = $treffer[2];
+				$treffer[2] = $treffer[1];
+				$treffer[1] = $treffer['bak'];
+				unset($treffer['bak']);
+			}
+			$newdate['month'] = $treffer[2];
+			if ($treffer[1] > 31) {
+				$newdate['year'] = $treffer[1];
+				$newdate['day'] = $treffer[3];
+			} else {
+				$newdate['year'] = $treffer[3];
+				$newdate['day'] = $treffer[1];
+			}
+		} elseif (preg_match('/^([0-9]{1,4})$/', $datum)) {
+			$newdate['year'] = $datum;
+			while (strlen($newdate['year']) < 4)
+				$newdate['year'] = '0'.$newdate['year'];
+			$newdate['month'] = '00';
+			$newdate['day'] = '00';
+		} elseif (preg_match('/^([0-9]{1,4})[^0-9][ ]*([0-9]{1,4})$/', $datum, $treffer)) {
+			$newdate['day'] = '00';
+			if ($treffer[1] > 31) {
+				$newdate['year'] = $treffer[1];
+				$newdate['month'] = $treffer[2];
+			} else {
+				$newdate['month'] = $treffer[1];
+				$newdate['year'] = $treffer[2];
+			}
+		} else {
+			$datum = str_replace(' ', '', $datum);
+		}
+		if (!empty($newdate)) {
+			if (strlen($newdate['month']) == 1) $newdate['month'] = '0'.$newdate['month'];
+			if (strlen($newdate['day']) == 1) $newdate['day'] = '0'.$newdate['day'];
+			if (strlen($newdate['year']) == 3 || strlen($newdate['year']) == 1)
+				$newdate['year'] = '0'.$newdate['year'];
+			if ($newdate['year'] < 100 && strlen($newdate['year']) < 3)
+				$newdate['year'] = ($newdate['year'] > 70) ? '19'.$newdate['year'] : '20'.$newdate['year'];
+			return $newdate['year'].'-'.$newdate['month'].'-'.$newdate['day'];
+			// $isodatum = sprintf ("%04d-%02d-%02d", $jahr, $monat, $tag);
+		}
+		return false;
+	}	
+}
+
 ?>
