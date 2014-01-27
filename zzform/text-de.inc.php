ff3ebf6627de0839ce9c8d21df08150583074df5
Gustaf Mossakowski <web@koenige.org>
2014-01-27 01:21:23 +0000

    zzform text: changed copyright to include 2014
    git-svn-id: file:///Repos/zzform/trunk@987 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzform/text-de.inc.php
+++ zzform/text-de.inc.php
@@ -8,7 +8,7 @@
  * http://www.zugzwang.org/projects/zzform
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2005-2012 Gustaf Mossakowski
+ * @copyright Copyright © 2005-2014 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
