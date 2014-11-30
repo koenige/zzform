fff340976cb61937c9bc772dc3e012438784da49
Gustaf Mossakowski <web@koenige.org>
2014-11-30 13:47:44 +0000

    zzform: javascript for WMD editor will not be included in export mode
    git-svn-id: file:///Repos/zzform/trunk@1218 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzform/zzform.php
+++ zzform/zzform.php
@@ -335,7 +335,9 @@ function zzform($zz = array()) {
 	if ($ops['heading']) {
 		$ops['title'] = zz_nice_title($ops['heading'], $zz['fields'], $zz_var, $ops['mode']);
 	}
-	$ops['output'] .= zz_output_wmd_editor();
+	if ($ops['mode'] !== 'export') {
+		$ops['output'] .= zz_output_wmd_editor();
+	}
 	return zzform_exit($ops);
 }
 
