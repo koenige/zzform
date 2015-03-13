414cb713dcd66e2ad02f46221ce8f5c1adc0a23f
Gustaf Mossakowski <web@koenige.org>
2015-03-13 21:57:12 +0000

    zzform imagemagick: abort happens as well, if aborting… is the message
    git-svn-id: file:///Repos/zzform/trunk@1263 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzform/image-imagemagick.inc.php
+++ zzform/image-imagemagick.inc.php
@@ -76,6 +76,7 @@ function zz_imagick_identify($filename, $file) {
 	// e. g. Error: identify: mv:
 	$tokens = explode(' ', $output[0]);
 	if (substr($tokens[0], -1) === ':') return zz_return($file);
+	if (substr(($tokens[0], 0, 11) === 'aborting...') return zz_return($file);
 	$result = array_pop($output);
 	if (count($output)) {
 		// e. g.  '   **** Warning:', 'GPL Ghostscript:'
