57c049f805eb49fbd3463e7ec289576735114730
Gustaf Mossakowski <web@koenige.org>
2016-06-20 11:38:53 +0000

    zzform: fix syntax error in zz_check_select_id()
    git-svn-id: file:///Repos/zzform/trunk@1597 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzform/functions.inc.php
+++ zzform/functions.inc.php
@@ -2894,7 +2894,7 @@ function zz_check_select_id($field, $postvalue, $db_table, $id = array()) {
 	global $zz_conf;
 	
 	// 1. get field names from SQL query
-	$sql_fieldnames = wrap_edit_sql($field['sql'], 'SELECT', '', 'list'));
+	$sql_fieldnames = wrap_edit_sql($field['sql'], 'SELECT', '', 'list');
 	foreach ($sql_fieldnames as $index => $sql_fieldname) {
 		if (!empty($field['show_hierarchy'])
 			AND $sql_fieldname['field_name'] !== $field['show_hierarchy']) {
