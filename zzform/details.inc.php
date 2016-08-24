baad2f5f795ff8c872cd666b46049f188fbc7a4e
Gustaf Mossakowski <web@koenige.org>
2016-08-24 10:23:02 +0000

    details: remove copied function head which does not belong to these functions
    git-svn-id: file:///Repos/zzform/trunk@1673 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzform/details.inc.php
+++ zzform/details.inc.php
@@ -7,11 +7,6 @@
  * Part of »Zugzwang Project«
  * http://www.zugzwang.org/projects/zzform
  *
- * List of functions in this file
- *	zzform()				main zzform function
- *		zz_initialize()		sets defaults, imports modules, reads URI
- *	zzform_multi()			multi edit for zzform, e. g. import
- *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
  * @copyright Copyright © 2004-2016 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
