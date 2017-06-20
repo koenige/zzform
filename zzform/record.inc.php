69aee97e4ab3c09ccbf4801d53300ae34f25c045
Gustaf Mossakowski <web@koenige.org>
2017-06-20 22:08:56 +0000

    record: bugfix in zz_show_field_rows() for 'access' = 'all', do not set 'all' for $field_display at all
    git-svn-id: file:///Repos/zzform/trunk@1822 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzform/record.inc.php
+++ zzform/record.inc.php
@@ -531,8 +531,7 @@ function zz_show_field_rows($zz_tab, $mode, $display, &$zz_var, $zz_conf_record,
 		}
 		
 		if ($field['type'] === 'subtable') {
-			$field_display = !empty($field['access']) ? $field['access'] : $display;
-			if ($field_display === 'all') $field_display = 'form';
+			$field_display = (!empty($field['access']) AND $field['access'] !== 'all') ? $field['access'] : $display;
 			if (empty($field['form_display'])) $field['form_display'] = 'vertical';
 			if (!empty($field['hierarchy'])) $field['form_display'] = 'horizontal';
 		} else {
