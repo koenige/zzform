2a658825a40aa27ecf026546a9ac72217072d848
Gustaf Mossakowski <web@koenige.org>
2018-04-22 08:56:54 +0000

    geo: fix code comments
    git-svn-id: file:///Repos/zzform/trunk@1966 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzform/geo.inc.php
+++ zzform/geo.inc.php
@@ -259,7 +259,7 @@ function zz_geo_coord_sql_out($point, $out = false, $concat = ', ') {
  * formats a number depending on language with . or ,
  *
  * @param string $number
- * @global array $zz_conf ($zz_conf['language'])
+ * @global array $zz_conf ($zz_conf['decimal_point'])
  * @return string $number
  */
 function zz_decimal($number) {
