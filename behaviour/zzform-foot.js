167b8e3af56809d013e298d6ed1f5bd607e24b5d
Gustaf Mossakowski <gustaf@koenige.org>
2018-08-30 17:04:29 +0000

    reflect Firefox' changes (not supporting styling of option elements) and use JS for Safari for all browser for indentation
    git-svn-id: file:///Repos/zzform/trunk@2023 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- behaviour/zzform-foot.js
+++ behaviour/zzform-foot.js
@@ -13,20 +13,18 @@
 
 /**
  *	adds &nbsp; for hierarchical form options
- *	Safari unfortunately does not support modifications to style of forms
+ *	Safari and since 2018 Firefox as well
+ *  unfortunately does not support modifications to style of forms
  */
-var isSafari = navigator.userAgent.search(/Safari.+/);
-if (isSafari != -1) {
-	var optionfields = document.getElementsByTagName('option');
-	for (var i=0; i<optionfields.length; i++) {
-		if (optionfields[i].className.substring(0, 5) == 'level') {
-			var len = optionfields[i].className.substring(5, 6);
-			var level = '';
-			for (var k=0; k<len; k++) {
-				level = level + String.fromCharCode(160) + String.fromCharCode(160) + String.fromCharCode(160) + String.fromCharCode(160);
-			}
-			optionfields[i].text = level + optionfields[i].text;
+var optionfields = document.getElementsByTagName('option');
+for (var i=0; i<optionfields.length; i++) {
+	if (optionfields[i].className.substring(0, 5) == 'level') {
+		var len = optionfields[i].className.substring(5, 6);
+		var level = '';
+		for (var k=0; k<len; k++) {
+			level = level + String.fromCharCode(160) + String.fromCharCode(160) + String.fromCharCode(160) + String.fromCharCode(160);
 		}
+		optionfields[i].text = level + optionfields[i].text;
 	}
 }
 
