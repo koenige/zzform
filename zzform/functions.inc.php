821f52f7822827c62ad563e1cebb5ce9593533f3
Gustaf Mossakowski <gustaf@koenige.org>
2019-04-03 09:22:10 +0000

    add parameter explanation_vars to set a variable for an explanation that is injected after translating the explanation
    git-svn-id: file:///Repos/zzform/trunk@2124 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzform/functions.inc.php
+++ zzform/functions.inc.php
@@ -949,7 +949,12 @@ function zz_fill_out($fields, $db_table, $multiple_times = false, $mode = false,
 		}
 
 		if (!isset($fields[$no]['explanation'])) {
-			$fields[$no]['explanation'] = false;
+			$fields[$no]['explanation'] = '';
+		}
+		if (!empty($fields[$no]['explanation_vars'])) {
+			if (!is_array($fields[$no]['explanation_vars']))
+				$fields[$no]['explanation_vars'] = [$fields[$no]['explanation_vars']];
+			$fields[$no]['explanation'] = vsprintf($fields[$no]['explanation'], $fields[$no]['explanation_vars']);
 		}
 		if (!$multiple_times) {
 			if (!empty($fields[$no]['sql'])) // replace whitespace with space
