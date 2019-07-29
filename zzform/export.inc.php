1c7b7060065afc800bd2ccb941e06d1113ac6079
Gustaf Mossakowski <gustaf@koenige.org>
2019-07-29 10:51:35 +0000

    export: only mask export fields if csv_enclosure is not an empty string
    git-svn-id: file:///Repos/zzform/trunk@2164 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzform/export.inc.php
+++ zzform/export.inc.php
@@ -557,7 +557,7 @@ function zz_export_csv_body($rows) {
 			}
 			if ($myfield) {
 				$mask = false;
-				if ($zz_conf['list_display'] === 'csv-excel') {
+				if ($zz_conf['list_display'] === 'csv-excel' AND $zz_conf['export_csv_enclosure']) {
 					if (preg_match('/^0[0-9]+$/', $myfield)) {
 					// - number with leading 0 = TEXT
 						$mask = true;
