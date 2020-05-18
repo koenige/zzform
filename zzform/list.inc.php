ead440c20936213f86509f382ddb948ebc77393b
Gustaf Mossakowski <gustaf@koenige.org>
2020-05-18 22:30:39 +0000

    list: do not show add links above if there are less than 8 rows on page
    git-svn-id: file:///Repos/zzform/trunk@2304 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzform/list.inc.php
+++ zzform/list.inc.php
@@ -85,8 +85,6 @@ function zz_list($zz, $ops, $zz_var, $zz_conditions) {
 	$zz['sql'] = zz_list_filter_sql($zz['filter'], $zz['sql'], $zz_var['filters']);
 	if (zz_list_filter_invalid()) $zz['sql'] = false;
 	if ($old_sql !== $zz['sql']) $zz['sqlcount'] = '';
-	$ops['output'] .= zz_output_add_export_links($zz, $ops, 'above');
-	$ops['output'] .= zz_filter_selection($zz['filter'], $zz_var['filters'], 'top');
 	if (!$zz['sql']) return zz_return([$ops, $zz_var]);
 
 	list($lines, $ops['records_total']) = zz_list_query($zz, $id_field);
@@ -94,6 +92,10 @@ function zz_list($zz, $ops, $zz_var, $zz_conditions) {
 	$zz_var['limit_total_rows'] = $ops['records_total'];
 	if (zz_error_exit()) return zz_return([$ops, $zz_var]);
 	$count_rows = count($lines);
+
+	if ($count_rows < 8) $zz_conf['no_add_above'] = true;
+	$ops['output'] .= zz_output_add_export_links($zz, $ops, 'above');
+	$ops['output'] .= zz_filter_selection($zz['filter'], $zz_var['filters'], 'top');
 
 	// don't show anything if there is nothing
 	if (!$count_rows) {
