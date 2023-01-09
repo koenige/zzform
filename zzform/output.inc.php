ace83c505cc9a298675ef31be706a0d69804fb19
Gustaf Mossakowski <gustaf@koenige.org>
2023-01-09 12:19:32 +0000

    output, title: before stripping HTML tags, replace line break with ": "
    git-svn-id: file:///Repos/zzform/trunk@2834 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzform/output.inc.php
+++ zzform/output.inc.php
@@ -9,7 +9,7 @@
  * https://www.zugzwang.org/projects/zzform
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2004-2022 Gustaf Mossakowski
+ * @copyright Copyright © 2004-2023 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -417,7 +417,8 @@ function zz_nice_title($heading, $fields, $ops, $mode = false) {
 	global $zz_conf;
 
 	// basic title
-	$title = strip_tags($heading);
+	$title = str_replace('<br>', ': ', $heading);
+	$title = strip_tags($title);
 
 	// addition: filters
 	if (!empty($ops['filter_titles'])) {
