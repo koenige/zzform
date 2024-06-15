9519f5b356b98837ab2f37347ea3e745d2952095
Gustaf Mossakowski <gustaf@koenige.org>
2024-06-15 14:18:59 +0000

    sync make: extract reading queries into separate function zz_sync_queries(), read per identifier
    git-svn-id: file:///Repos/zzform/trunk@3280 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_make/sync.inc.php
+++ zzbrick_make/sync.inc.php
@@ -21,27 +21,10 @@
  */
 function mod_zzform_make_sync($params) {
 	wrap_setting('log_username_suffix', 'Sync');
-	
-	$queries = [];
-	$files = wrap_collect_files('configuration/sync.sql');
-	foreach ($files as $file)
-		$queries = array_merge_recursive($queries, wrap_sql_file($file, '_'));
-	foreach ($queries as $identifier => $module_queries) {
-		$ids = [];
-		foreach ($module_queries as $key => $query) {
-			if (!str_starts_with($key, 'static')) continue;
-			if (!strstr($query, ' = /*_ID')) continue;
-			list($qkey, $qvalue) = explode(' = ', $query);
-			$ids[] = $qkey;
-		}
-		$queries[$identifier] = wrap_sql_placeholders($queries[$identifier]);
-		if ($ids)
-			$queries[$identifier]['ids'] = $ids;
-	}
 
 	$data = wrap_cfg_files('sync');
 	foreach (array_keys($data) as $identifier) {
-		$data[$identifier] += $queries[$identifier];
+		$data[$identifier] += zz_sync_queries($identifier);
 		$data[$identifier]['identifier'] = $identifier;
 	}
 
@@ -49,11 +32,41 @@ function mod_zzform_make_sync($params) {
 		$data = array_values($data);
 		$page['text'] = wrap_template('sync-overview', $data);
 		return $page;
-	} 
+	}
 
 	wrap_include('sync', 'zzform');
 	wrap_include('zzform/definition');
 	$page = zz_sync($data[$params[0]]);
 	$page['breadcrumbs'][]['title'] = $data[$params[0]]['title'];
 	return $page;
+}
+
+/**
+ * read data from sync.sql
+ *
+ * @param string $identifier
+ * @return array
+ */
+function zz_sync_queries($identifier) {
+	$queries = [];
+	$files = wrap_collect_files('configuration/sync.sql');
+	foreach ($files as $file)
+		$queries = array_merge_recursive($queries, wrap_sql_file($file, '_'));
+
+	if (!array_key_exists($identifier, $queries)) return [];
+	$queries = $queries[$identifier];
+
+	// get ids
+	$ids = [];
+	foreach ($queries as $key => $query) {
+		if (!str_starts_with($key, 'static')) continue;
+		if (!strstr($query, ' = /*_ID')) continue;
+		list($qkey, $qvalue) = explode(' = ', $query);
+		$ids[] = $qkey;
+	}
+	$queries = wrap_sql_placeholders($queries);
+	if ($ids)
+		$queries['ids'] = $ids;
+
+	return $queries;
 }
