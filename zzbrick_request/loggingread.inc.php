dd32bbd463dd9da496b6e0ab7bc8b679a4bc76f7
Gustaf Mossakowski <gustaf@koenige.org>
2024-07-20 09:42:11 +0000

    loggingread: add request script to read logging entries, moved here from default module, zz_maintenance_sqldownload()
    git-svn-id: file:///Repos/zzform/trunk@3342 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ zzbrick_request/loggingread.inc.php
@@ -0,0 +1,44 @@
+<?php 
+
+/**
+ * default module
+ * export SQL log as JSON file
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/default
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2016-2024 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+/**
+ * export SQL log as JSON file
+ *
+ * @param array $params
+ * @return array
+ */
+function mod_zzform_loggingread($params) {
+	wrap_include('logging', 'zzform');
+	list($data, $limit) = zz_logging_read($params[0]);
+	if (!$data) {
+		$sql = 'SELECT MAX(log_id) FROM %s';
+		$sql = sprintf($sql, wrap_sql_table('zzform_logging'));
+		$data['max_logs'] = wrap_db_fetch($sql, '', 'single value');
+		$page['title'] = ' '.wrap_text('Download SQL log');
+		$page['breadcrumbs'][]['title'] = wrap_text('Download SQL log');
+		$page['text'] = wrap_template('logging-read', $data);
+		return $page;
+	}
+
+	$page['text'] = json_encode($data);
+	$page['query_strings'][] = 'loggingread'; // @todo change this, here for call from maintenance script
+	$page['content_type'] = 'json';
+	if ($limit) {
+		$page['headers']['filename'] = sprintf('logging_%d-%d.json', $params[0], $params[0] + $limit - 1);
+	} else {
+		$page['headers']['filename'] = sprintf('logging_%d.json', $params[0]);
+	}
+	return $page;
+}
