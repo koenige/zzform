d701d93a1bf48df683ba5d62ed266b53f339f282
Gustaf Mossakowski <gustaf@koenige.org>
2024-07-20 09:18:05 +0000

    loggingadd: add make script to add logging entries, moved here from default module, zz_maintenance_sqlupload()
    git-svn-id: file:///Repos/zzform/trunk@3341 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ zzbrick_make/loggingadd.inc.php
@@ -0,0 +1,37 @@
+<?php 
+
+/**
+ * zzform module
+ * add logging data
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/projects/zzform
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2016-2024 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+/**
+ * export JSON file in _logging
+ *
+ * @param array $page
+ * @return array $page
+ */
+function mod_zzform_make_loggingadd() {
+	$data = [];
+	if (empty($_FILES['sqlfile'])) $data['no_file'] = true;
+	elseif ($_FILES['sqlfile']['error'] === UPLOAD_ERR_NO_FILE) $data['no_file'] = true;
+	elseif ($_FILES['sqlfile']['error'] !== 0) $data['file_error'] = true;
+	elseif ($_FILES['sqlfile']['size'] <= 3) $data['file_error'] = true;
+	else {
+		wrap_include('logging', 'zzform');
+		$json = file_get_contents($_FILES['sqlfile']['tmp_name']);
+		$data = zz_logging_add($json);
+	}
+	$page['title'] = wrap_text('Upload SQL log');
+	$page['breadcrumbs'][]['title'] = wrap_text('Upload SQL log');
+	$page['text'] = wrap_template('logging-add', $data);
+	return $page;
+}
