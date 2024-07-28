6dfeac480491dbc955ca6f649d25d0c3c79b31a8
Gustaf Mossakowski <gustaf@koenige.org>
2024-07-28 22:48:28 +0000

    serversync: move development part of serversync from default module to zzform module, make serversync
    git-svn-id: file:///Repos/zzform/trunk@3346 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_make/serversync.inc.php
+++ zzbrick_make/serversync.inc.php
@@ -55,5 +55,81 @@ function mod_zzform_make_serversync_production() {
 		}
 		return $page;
 	}
-	wrap_quit(403, wrap_text('This URL is for synchronising a production and a development server only. No direct access is possible.'));
+	wrap_quit(403, wrap_text(
+		'This URL is for synchronising a production and a development server only. No direct access is possible.'
+	));
+}
+
+/**
+ * Synchronisation on development server
+ *
+ * @return array
+ */
+function mod_zzform_make_serversync_development() {
+	wrap_include('logging', 'zzform');
+	$page['title'] = wrap_text('Synchronize local and remote server');
+	wrap_setting('log_username_default', wrap_setting('sync_user'));
+	
+	if (!wrap_setting('local_access')) {
+		$out['local_only'] = true;
+		$page['text'] = wrap_template('sync-server', $out);
+		return $page;
+	}
+
+	$path = wrap_path('default_sync_server');
+	$url = sprintf('https://%s%s', substr(wrap_setting('hostname'), 0, -6), $path);
+	$data = ['return_last_logging_entry' => 1];
+	$headers_to_send[] = 'Accept: application/json';
+	list($status, $headers, $content) = wrap_get_protected_url($url, $headers_to_send, 'POST', $data);
+	
+	if ($status !== 200) {
+		$out['status_error'] = $status;
+		$content = json_decode($content, true);
+		$out['error_explanation'] = $content['error_explanation'] ?? '';
+		$page['text'] = wrap_template('sync-server', $out);
+		return $page;
+	}
+	$last_log = json_decode($content, true);
+	$last_log_local = zz_logging_last();
+	if ($last_log === $last_log_local) {
+		$out['identical'] = wrap_number($last_log['log_id']);
+	} elseif ($last_log['log_id'] < $last_log_local['log_id']) {
+		// push data from local server
+		list($log, $limit) = zz_logging_read($last_log['log_id'] + 1);
+		$data = [];
+		$data['add_log'] = json_encode($log, true);
+		list($status, $headers, $content) = wrap_get_protected_url($url, $headers_to_send, 'POST', $data);
+		if ($status !== 200) {
+			$out['status_error'] = $status;
+			$content = json_decode($content, true);
+			$out['error_explanation'] = $content['error_explanation'] ?? '';
+			$page['text'] = wrap_template('sync-server', $out);
+			return $page;
+		}
+		$out = json_decode($content, true);
+		$out['hide_upload_form'] = true;
+		$out['remote_changes'] = true;
+		$page['text'] = wrap_template('logging-add', $out);
+		return $page;
+	} elseif ($last_log['log_id'] > $last_log_local['log_id']) {
+		// get data from remote server
+		$url .= sprintf('?get_log_from_id=%d', $last_log_local['log_id'] + 1);
+		list($status, $headers, $content) = wrap_get_protected_url($url, $headers_to_send);
+		if ($status !== 200) {
+			$out['status_error'] = $status;
+			$content = json_decode($content, true);
+			$out['error_explanation'] = $content['error_explanation'] ?? '';
+			$page['text'] = wrap_template('sync-server', $out);
+			return $page;
+		}
+		$out = zz_logging_add($content);
+		$out['hide_upload_form'] = true;
+		$out['local_changes'] = true;
+		$page['text'] = wrap_template('logging-add', $out);
+		return $page;
+	} else {
+		$out['mismatch'] = $last_log['log_id'];
+	}
+	$page['text'] = wrap_template('sync-server', $out);
+	return $page;
 }
