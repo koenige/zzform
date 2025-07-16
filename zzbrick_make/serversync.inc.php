f26383ef4cd53bdc2c369caebf8c4db72fce7f52
Gustaf Mossakowski <gustaf@koenige.org>
2025-07-16 21:19:51 +0000

    serversync: use data array instead of JSON-encoded text now that wrap_page_json() is used
    git-svn-id: file:///Repos/zzform/trunk@3679 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_make/serversync.inc.php
+++ zzbrick_make/serversync.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/zzform
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2018, 2020-2024 Gustaf Mossakowski
+ * @copyright Copyright © 2018, 2020-2025 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -33,19 +33,19 @@ function mod_zzform_make_serversync_production() {
 	
 	if (!empty($_POST['return_last_logging_entry'])) {
 		$data = zz_logging_last();
-		$page['text'] = json_encode($data, true);
+		$page['data'] = $data;
 		$page['content_type'] = 'json';
 		$page['headers']['filename'] = 'logging_last.json';
 		return $page;
 	} elseif (!empty($_POST['add_log'])) {
 		$out = zz_logging_add($_POST['add_log']);
-		$page['text'] = json_encode($out, true);
+		$page['data'] = $out;
 		$page['content_type'] = 'json';
 		$page['headers']['filename'] = 'logging_add.json';
 		return $page;
 	} elseif (!empty($_GET['get_log_from_id'])) {
 		list($log, $limit) = zz_logging_read($_GET['get_log_from_id']);
-		$page['text'] = json_encode($log, true);
+		$page['data'] = $log;
 		$page['query_strings'] = ['get_log_from_id'];
 		$page['content_type'] = 'json';
 		if ($limit) {
