5802f2d4f04a8324a5bf9e0cb547f5ce962f6283
Gustaf Mossakowski <gustaf@koenige.org>
2026-08-16 10:57:11 +0200

    settings: add parameter setting `zzform_if`, use `zzform_if[context]` instead of `if[context][zzform]`

--- configuration/update.sql
+++ configuration/update.sql
@@ -25,3 +25,4 @@
 /* 2026-08-11-1 */	DELETE FROM _settings WHERE setting_key = 'zzform_logging_id';
 /* 2026-08-16-4 */	UPDATE categories SET parameters = REPLACE(parameters, '&zzform_def[', '&zzform[') WHERE parameters LIKE '%&zzform_def[%';
 /* 2026-08-16-5 */	UPDATE categories SET parameters = REPLACE(parameters, '[zzform_def][', '[zzform][') WHERE parameters LIKE '%[zzform_def][%';
+/* 2026-08-16-7 */	UPDATE categories SET parameters = REGEXP_REPLACE(parameters, '&if\\[([^&\\]]+)\\]\\[zzform\\]\\[', '&zzform_if[\\1][', 1, 0) WHERE parameters LIKE '%&if[%' AND parameters LIKE '%[zzform][%';
